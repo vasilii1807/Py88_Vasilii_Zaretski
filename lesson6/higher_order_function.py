adc2828365f4d43f634d44d9534f9d8ff0319da6
Vasili Zaretski <vasilii147258@gmail.com>
2023-04-17 21:45:26 +0300

    update higher_order_function.py

--- lesson6/higher_order_function.py
+++ lesson6/higher_order_function.py
@@ -1,19 +1,24 @@
-
 def first_function():
     """print first_function"""
     return "first_function"
+
 
 def second_function():
     """print second_function"""
     return "second_function"
 
+
 def third_function():
     """print third_function"""
     return "third_function"
 
-print(*map(lambda x: f'{x + 1} ' + first_function(), range(3)),sep = '\n')
-print()
-print(*map(lambda x: f'{x + 1} ' + second_function(), range(5)),sep = '\n')
-print()
-print(*map(lambda x: f'{x + 1} ' + third_function(), range(10)),sep = '\n')
 
+def high_func(func, n=10):
+    print(*map(lambda x: f'{x + 1} ' + first_function(), range(n)), sep='\n')
+    print()
+
+
+high_func(first_function(), 3)
+high_func(second_function(), 5)
+high_func(third_function(), 10)
+high_func(first_function())
