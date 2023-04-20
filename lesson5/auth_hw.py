413abe788fbf188e16805b0e653917411b97021c
Vasili Zaretski <vasilii147258@gmail.com>
2023-04-20 23:04:12 +0300

    update. pep8

--- lesson5/auth_hw.py
+++ lesson5/auth_hw.py
@@ -1,4 +1,5 @@
-usr_pass = {"Dima":"dima123", "Ivan":"ivan123", "Pavel":"pavel123", "Petr":"petr123", "Denis":"denis123"}
+usr_pass = {"Dima": "dima123", "Ivan": "ivan123", "Pavel": "pavel123", "Petr": "petr123", "Denis": "denis123"}
+
 
 def get_name():
     while True:
@@ -11,18 +12,21 @@ def get_name():
             break
     return user_name
 
+
 def get_password():
     user_password = input("Введите ваш пароль: ")
     return user_password
 
-def check_password(user_name,user_password):
+
+def check_password(user_name, user_password):
     while True:
         if user_password == usr_pass[user_name]:
             print(f"Hey {user_name}")
             break
         else:
             print("Authentication Error. Check name or password! ")
-            check_password(get_name(),get_password())
+            check_password(get_name(), get_password())
             break
 
-check_password(get_name(),get_password())
+
+check_password(get_name(), get_password())
